9562adb1895d418b4d46fc4b6a602473bd4d9956
Onur Osman Güler <guleronurosman@gmail.com>
2020-07-14 22:01:57 +0300

    Create Conversation & Message models

--- src/scobi_messenger/apps/chat/models.py
+++ src/scobi_messenger/apps/chat/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from scobi_messenger.apps.accounts.models import User
+
+
+class Conversation(models.Model):
+    users = models.ManyToManyField(User, related_name="conversations")
+
+    def __str__(self):
+        return '{}'.format(self.pk)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="messages")
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%(sender)s at %(conversation)s on %(created_at)s" % {
+            'sender': self.sender.username,
+            'conversation': self.conversation,
+            'created_at': self.created_at
+        }
